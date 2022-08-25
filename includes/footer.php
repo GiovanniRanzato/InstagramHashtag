fcb1d48696e16d96270a892d3b97d8e92f8d6c30
Giovanni Ranzato <giovanni.ranzato2010@gmail.com>
2022-08-25 19:38:01 +0200

    UPDATE - deleted unused files

--- includes/footer.php
+++ includes/footer.php
@@ -13,7 +13,6 @@
     }
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="js/fancybox/dist/jquery.fancybox.min.js"></script>
     <script src="js/main.js"></script>
 </body>
 </html>
